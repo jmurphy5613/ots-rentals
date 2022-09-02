bf0b0dc0b64316d39ca91fe150c6720ff12a5223
jmurphy5613 <jmurphy5613@gmail.com>
2022-09-02 01:46:50 -0400

    created the circular numbers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/navbar/Navbar'
+import DetailBoxes from '../components/detail-boxs/DetailBoxes'
 
 const Home: NextPage = () => {
   return (
     <>
       <Navbar />
       <div className={styles.container}>
-        <video autoPlay muted loop className={styles.video}>
+        <video muted loop className={styles.video}>
           <source src="/video.mp4" type="video/mp4" />
         </video>
+        <DetailBoxes />
       </div>
     </>
 
