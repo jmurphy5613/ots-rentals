abaa23aaceb8266466c9b2449bb4cf00bfb57607
jmurphy5613 <jmurphy5613@gmail.com>
2022-10-20 10:22:27 -0400

    set up popup

---
+++ components/dashboard/edit-gear-popup/EditGearPopup.tsx
@@ -0,0 +1,14 @@
+import styles from './EditGearPopup.module.css'
+
+
+const EditGearPopup = () => {
+
+
+    return (
+        <div className={styles["popup-container"]}>
+
+        </div>
+    )
+}
+
+export default EditGearPopup
