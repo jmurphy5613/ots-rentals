e920aa0a7ca79ba96dad39d4f4188654464ca2af
jmurphy5613 <jmurphy5613@gmail.com>
2022-10-25 11:54:42 -0400

    set up checkout button

--- components/user-view/shopping-cart/shopping-cart.tsx
+++ components/user-view/shopping-cart/shopping-cart.tsx
@@ -8,11 +8,14 @@ const ShoppingCart:React.FC<shoppingCartProps> = ({setShowCart}) => {
 
     return (
         <div className={styles.cart}>
-            <div className={styles.header}>
-                <h4 className={styles["cart-quantity"]}>Cart (1)</h4>
-                <h4 className={styles.exit} onClick={() => {
-                    setShowCart(false)
-                }}>x</h4>
+            <div className={styles.content}>
+                <div className={styles.header}>
+                    <h4 className={styles["cart-quantity"]}>Cart (1)</h4>
+                    <h4 className={styles.exit} onClick={() => {
+                        setShowCart(false)
+                    }}>x</h4>
+                </div>
+                <button className={styles.checkout}>Checkout</button>
             </div>
         </div>
     )
