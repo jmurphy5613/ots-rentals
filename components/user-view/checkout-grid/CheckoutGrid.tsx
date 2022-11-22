e13450632674944feab4f2521c208e8995405fda
jmurphy5613 <jmurphy5613@gmail.com>
2022-11-22 17:19:15 -0500

    added dyanmic rendering for checkout depending on if the user is authorized, added remove ability in checkout

--- components/user-view/checkout-grid/CheckoutGrid.tsx
+++ components/user-view/checkout-grid/CheckoutGrid.tsx
@@ -1,5 +1,7 @@
 import styles from './CheckoutGrid.module.css'
 import { Gear } from '../../../utils/types'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCartItems } from '../../../redux/features/cart'
 
 
 type CheckoutGrid = {
@@ -7,23 +9,36 @@ type CheckoutGrid = {
 }
 
 const CheckoutGrid:React.FC<CheckoutGrid> = ({ gear }) => {
+
+    const cart = useSelector((state: any) => state.cart.value)
+
+    const dispatch = useDispatch()
+
+    const deleteCartIndex = (index:number) => {
+        const currentCartItems = cart.items
+        const copy = [...currentCartItems]
+        copy.splice(index, 1)
+        dispatch(setCartItems({items: copy }))
+
+    }
+
     return (
         <div className={styles.grid}>
             {gear.map((element:Gear, index:number) => {
                 return (
                     <div className={styles["grid-item"]}>
                         <div onClick={() => {
-                            
+                            deleteCartIndex(index)
                         }} className={styles["red-circle"]}>
                             <h2 className={styles.x}>x</h2>
                         </div>
                         <img className={styles.image} src={element.picture} />
                         <h3 className={styles.name}>{element.name}</h3>
-                        <div className={styles["quantity-select"]}>
+                        {/* <div className={styles["quantity-select"]}>
                             <button className={styles.subtract}>-</button>
                             <h4 className={styles.number}>1</h4>
                             <button className={styles.add}>+</button>
-                        </div>
+                        </div> */}
                     </div>
                 )
             })}
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,11 +1,18 @@
 import styles from '../styles/Checkout.module.css'
 import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
-import { Gear } from '../utils/types'
+import { Gear, User } from '../utils/types'
 import CheckoutGrid from '../components/user-view/checkout-grid/CheckoutGrid'
+import { useUser } from '@auth0/nextjs-auth0'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 
 const Checkout = () => {
+
+    const { user, isLoading } = useUser()
+
+    const [isAuthorized, setIsAuthorized] = useState(false)
 
     const router = useRouter()
 
@@ -19,6 +26,26 @@ const Checkout = () => {
         return gear
     }
 
+    const handleOrders = () => {
+        const gear = createGearArray()
+
+        for(const item of gear) {
+            console.log(item)
+        }
+    }
+
+    useEffect(() => {
+        if(!user) return
+        axios.get(`http://localhost:3002/user/get-by-email/${user.email}`).then(res => {
+            if(res.data) {
+                console.log("hey")
+                setIsAuthorized(true)
+            }
+        })
+    }, [user]) 
+
+    if(isLoading) return <div></div>
+
     return (
         <div className={styles.container}>
             <h2 className={styles.back} onClick={() => {
@@ -28,10 +55,17 @@ const Checkout = () => {
             <CheckoutGrid gear={createGearArray()} />
 
             
-            {/* <div className={styles["code-container"]}>
-                <input className={styles.code} placeholder="Enter Code" />
-                <button className={styles.redeem}>Redeem</button>
-            </div> */}
+            {
+            isAuthorized 
+                ? 
+                    <div className={styles["code-container"]}>
+                        <button onClick={() => {
+                            handleOrders()
+                        }} className={styles.redeem}>Checkout</button>
+                    </div> 
+                :
+                    <h2 className={styles.warning}>You are not permited to checkout these items.</h2>
+            }
         </div>
     )
 }
