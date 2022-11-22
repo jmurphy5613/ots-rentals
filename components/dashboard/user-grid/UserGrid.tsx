16acae925194f5ffa034a18ddb7eee516dfb9096
jmurphy5613 <jmurphy5613@gmail.com>
2022-11-22 17:33:20 -0500

    added remove buttons to users

--- components/dashboard/user-grid/UserGrid.tsx
+++ components/dashboard/user-grid/UserGrid.tsx
@@ -12,6 +12,9 @@ const UserGrid:React.FC<UserGridProps> = ({ users }) => {
             {users.map((element:User, index) => {
                 return (
                     <div className={styles.user}>
+                        <div className={styles["remove-background"]}>
+                            <h4 className={styles.remove}>x</h4>
+                        </div>
                         <h1 className={styles.name}>{element.name}</h1>
                         <h2 className={styles.email}>{element.email}</h2>
                     </div>
